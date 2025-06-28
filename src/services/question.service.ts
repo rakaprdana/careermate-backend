d61b15a07813947c89ba88be5e72d4cfa8434121
rakaprdana <raka.pradana1103@gmail.com>
2025-06-28 11:17:01 +0700

    fix: response question service

--- src/services/question.service.ts
+++ src/services/question.service.ts
@@ -5,6 +5,6 @@ export class QuestionService {
     const skip = (page - 1) * limit;
     const data = await Question.find().skip(skip).limit(limit);
     const total = await Question.countDocuments();
-    return { data, total, page, pages: Math.ceil(total / limit) };
+    return { question: data, total, page, pages: Math.ceil(total / limit) };
   };
 }
