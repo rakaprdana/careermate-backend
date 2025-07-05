840cdc98cc2660a825a841aa314cf25b3b9aee23
rakaprdana <raka.pradana1103@gmail.com>
2025-07-05 17:12:37 +0700

    fix: fixed CORS Policy

--- src/app.ts
+++ src/app.ts
@@ -1,9 +1,12 @@
 import dotenv from "dotenv";
 import express from "express";
+import cors from "cors";
 import QuestionRoute from "../src/routes/question.route";
+import corsOptions from "./config/cors";
 dotenv.config();
 
 const app = express();
 app.use(express.json());
+app.use(cors(corsOptions));
 app.use("/api/question", QuestionRoute);
 export default app;
